8f9ef12454d85a85e5f5066ff2ecb31fe4b6e5fa
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-01-01 15:39:03 +0800

    平均數和標準差的運算套件 numPy

---
+++ numPy.py
@@ -0,0 +1,10 @@
+Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import numpy as np
+>>> myWeights = [51, 46.5, 50.3, 48, 49]
+>>> print(np.std(myWeights))
+1.6082288394379696
+>>> print(np.mean(myWeights))
+48.96
+>>> 
